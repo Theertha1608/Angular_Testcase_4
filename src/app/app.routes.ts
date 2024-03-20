e1d0a1354ace8665b679b19dab798f9e26d86085
theetha1608 <theerthakunjusss@gmail.com>
2024-03-20 17:48:46 +0530

    Finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: HomeComponent,
+        path:'', 
+    },
+    {
+        component: HomeComponent, 
+        path:'home-component',
+    },
+    {
+        component: AboutComponent,
+        path:'about-component',
+    },
+    {
+        component: ProductComponent,
+        path:'product-component',
+    },
+    {
+        component: ContactComponent,
+        path:'contact-component',
+    },
+];
