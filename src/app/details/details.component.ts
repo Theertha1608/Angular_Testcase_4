5df65b6a84ebe13d4a1ddab127d98ffabf616413
theetha1608 <theerthakunjusss@gmail.com>
2024-03-26 23:36:46 +0530

    Finished

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductComponent } from '../product/product.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
+  product: any; 
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const productId = +params['id']; 
+      this.product = ProductComponent.products.find(product => product.id === productId);
+    });
+  }
 
 }
+
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterOutlet,RouterLink, RouterModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
-  products = [
+  static products = [
     { id: 1, name: 'Product 1', description: 'Description of Product 1', price: 19.99 },
     { id: 2, name: 'Product 2', description: 'Description of Product 2', price: 24.99 },
   ];
+
+  getProducts(){
+    return ProductComponent.products;
+  }
 }
